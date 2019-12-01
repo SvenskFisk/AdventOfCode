57da4a4b7d008632f7a4f39a4ad2c47663cbbd12
Jonas Jämtberg <jonas@jamtberg.se>
2019-12-01 14:36:42 +0100

    day 1

---
+++ 2019/01/A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var modules = File.ReadAllLines("input.txt")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(int.Parse)
+                .Select(x => new {
+                    Weight = x,
+                    Fuel =  x / 3 - 2
+                })
+                .ToArray();
+
+            var totalFuel = modules.Sum(x => x.Fuel);
+
+            System.Console.WriteLine($"Total fuel required: {totalFuel}.");
+        }
+    }
+}
---
+++ 2019/01/B/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var modules = File.ReadAllLines("input.txt")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(int.Parse)
+                .Select(x => new {
+                    Weight = x,
+                    SimpleFuel =  x / 3 - 2,
+                    Fuel = RecursiveFuel(x / 3 - 2)
+                })
+                .ToArray();
+
+            var totalFuel = modules.Sum(x => x.Fuel);
+
+            System.Console.WriteLine($"Total fuel required: {totalFuel}.");
+        }
+
+        static int RecursiveFuel(int mass)
+        {
+            var newFuel = mass / 3 - 2;
+            if (newFuel > 0)
+            {
+                return mass + RecursiveFuel(newFuel);
+            }
+            else
+            {
+                return mass;
+            }
+        }
+    }
+}
