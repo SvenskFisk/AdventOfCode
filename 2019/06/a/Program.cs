95bcfe5eb801f43500d7c9e9164e3230802599c4
Jonas Jämtberg <jonas@jamtberg.se>
2019-12-06 16:28:27 +0100

    day 6

---
+++ 2019/06/a/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace a
+{
+    class Node
+    {
+        public Node(string name, string parent)
+        {
+            Name = name;
+            Parent = parent;
+        }
+
+        public string Name { get; }
+        public string Parent { get; }
+
+        public int Orbits { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = File.ReadAllLines("input.txt")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToDictionary(
+                    x => x.Substring(4, 3), 
+                    x => new Node(x.Substring(4, 3), x.Substring(0, 3)));
+
+            foreach (var node in nodes.Values)
+            {
+                node.Orbits = 1;
+                var current = node;
+                while (current.Parent != "COM")
+                {
+                    node.Orbits++;
+                    current = nodes[current.Parent];
+                }
+            }
+
+            var orbits = nodes.Values.Sum(x => x.Orbits);
+            System.Console.WriteLine(orbits);
+        }
+    }
+}
---
+++ 2019/06/b/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace b
+{
+    class Node
+    {
+        public Node(string name, string parent)
+        {
+            Name = name;
+            Parent = parent;
+        }
+
+        public string Name { get; }
+        public string Parent { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = File.ReadAllLines("input.txt")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToDictionary(
+                    x => x.Substring(4, 3),
+                    x => new Node(x.Substring(4, 3), x.Substring(0, 3)));
+            nodes.Add("COM", new Node("COM", null));
+
+            var santa = nodes[nodes["SAN"].Parent];
+            var santaCount = 0;
+            while (true)
+            {
+                var meCount = 0;
+                var me = nodes[nodes["YOU"].Parent];
+                while (me.Name != santa.Name && me.Parent != null)
+                {
+                    me = nodes[me.Parent];
+                    meCount++;
+                }
+
+                if (me.Name == santa.Name)
+                {
+                    System.Console.WriteLine(meCount + santaCount);
+                    break;
+                }
+
+                santa = nodes[santa.Parent];
+                santaCount++;
+            }
+        }
+    }
+}
