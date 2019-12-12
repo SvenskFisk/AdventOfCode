65de6d7388fa0317883b945696113974300e2d63
Jonas Jämtberg <jonas@jamtberg.se>
2019-12-12 22:24:54 +0100

    day 12

---
+++ 2019/12/a/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input =
+@"<x=5, y=-1, z=5>
+<x=0, y=-14, z=2>
+<x=16, y=4, z=0>
+<x=18, y=1, z=16>";
+
+            var bodies = input.Split("\r\n")
+                .Select(x => new Body
+                {
+                    Position = new IntVector3(
+                        int.Parse(Regex.Match(x, @"x=(-?\d+)").Groups[1].Value),
+                        int.Parse(Regex.Match(x, @"y=(-?\d+)").Groups[1].Value),
+                        int.Parse(Regex.Match(x, @"z=(-?\d+)").Groups[1].Value)),
+                    Velocity = IntVector3.Zero
+                })
+                .ToArray();
+
+            foreach (var b in bodies)
+            {
+                Console.WriteLine(b.ToString());
+            }
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var gs = bodies
+                    .Select(x => new
+                    {
+                        Body = x,
+                        G = bodies
+                            .Select(y => new IntVector3(
+                                y.Position.X.CompareTo(x.Position.X),
+                                y.Position.Y.CompareTo(x.Position.Y),
+                                y.Position.Z.CompareTo(x.Position.Z)))
+                            .Aggregate((a, n) => a + n)
+                    })
+                    .ToArray();
+
+                Console.WriteLine();
+                foreach (var g in gs)
+                {
+                    g.Body.Tick(g.G);
+                    Console.WriteLine(g.Body.ToString());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Energy: " + bodies.Sum(x => x.Energy));
+        }
+    }
+
+    class Body
+    {
+        public IntVector3 Position { get; set; }
+
+        public IntVector3 Velocity { get; set; }
+
+        public int Energy => Position.AbsSum * Velocity.AbsSum;
+
+        public void Tick(IntVector3 gravity)
+        {
+            Velocity += gravity;
+            Position += Velocity;
+        }
+
+        public override string ToString()
+        {
+            return $"pos=<{Position.X,2} {Position.Y,2} {Position.Z,2}> vel=<{Velocity.X,2} {Velocity.Y,2} {Velocity.Z,2}>";
+        }
+    }
+
+    [DebuggerDisplay("{X} {Y} {Z}")]
+    struct IntVector3
+    {
+        public IntVector3(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static IntVector3 Zero => new IntVector3(0, 0, 0);
+
+        public static IntVector3 operator +(IntVector3 a, IntVector3 b) => new IntVector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+
+        public static IntVector3 operator -(IntVector3 a, IntVector3 b) => new IntVector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public int Z { get; }
+
+        public int AbsSum => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+    }
+}
---
+++ 2019/12/b/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input =
+@"<x=5, y=-1, z=5>
+<x=0, y=-14, z=2>
+<x=16, y=4, z=0>
+<x=18, y=1, z=16>";
+
+            var pos = new[,] {
+                { 5,0,16,18},
+                { -1,-14,4,1},
+                { 5,2,0,16}
+            };
+
+            var posO = new[,] {
+                { 5,0,16,18},
+                { -1,-14,4,1},
+                { 5,2,0,16}
+            };
+
+            var vel = new[,] {
+                { 0,0,0,0},
+                { 0,0,0,0},
+                { 0,0,0,0}
+            };
+
+            var periods = new long[3];
+            for (int ii = 0; ii < 3; ii++) // loop over x,y,z
+            {
+                do
+                {
+                    for (int i = 0; i < 4; i++) // loop over bodies for one dimension
+                    {
+                        var p = pos[ii, i];
+                        vel[ii, i] +=
+                            pos[ii, 0].CompareTo(p) +
+                            pos[ii, 1].CompareTo(p) +
+                            pos[ii, 2].CompareTo(p) +
+                            pos[ii, 3].CompareTo(p);
+                    }
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        pos[ii, i] += vel[ii, i];
+                    }
+                    periods[ii]++;
+
+                    if (pos[ii, 0] == posO[ii, 0] && pos[ii, 1] == posO[ii, 1] && pos[ii, 2] == posO[ii, 2] && pos[ii, 3] == posO[ii, 3] &&
+                        vel[ii, 0] == 0 && vel[ii, 1] == 0 && vel[ii, 2] == 0)
+                    {
+                        Console.WriteLine($"{ii} period {periods[ii]}");
+                        break;
+                    }
+                } while (true);
+            }
+
+            var state = new long[3];
+            do
+            {
+                if (state[0] < state[1]) state[0] += periods[0];
+                else if (state[1] < state[2]) state[1] += periods[1];
+                else state[2] += periods[2];
+
+            } while (state[0] != state[1] || state[0] != state[2]);
+
+            Console.WriteLine("Final: " + state[0]);
+        }
+    }
+}
